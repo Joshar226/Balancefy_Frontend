850861777eaa4609911e52a7bbd6bd10f3f21219
Joshar226 <josharaya226@gmail.com>
2025-05-23 20:46:56 -0600

    Liabilities Responsive

--- src/components/liabilities/LiabilityCard.tsx
+++ src/components/liabilities/LiabilityCard.tsx
@@ -9,10 +9,11 @@ type LiabilityCardProps = {
 export default function LiabilityCard({liability} : LiabilityCardProps) {
 
   return (
-    <div className="bg-orange-600 p-5 rounded-xl flex justify-between">
-      <div className="space-y-1">
-      <h2 className="text-white font-bold uppercase text-xl">{liability.title}</h2>
-      <h3 className="text-white font-bold uppercase text-xl">{formatCurrency(+liability.value)}</h3>
+    <div className="bg-orange-600 p-3 rounded-xl flex justify-between
+      md:p-5">
+      <div className="space-y-1 max-w-[100px] flex-1">
+      <h2 className="text-white font-bold uppercase text-sm md:text-xl">{liability.title}</h2>
+      <h3 className="text-white font-bold uppercase text-sm md:text-xl">{formatCurrency(+liability.value)}</h3>
       </div>
       <LiabilityDropdownMenu id={liability._id} />
     </div>
--- src/views/app/LiabilitiesView.tsx
+++ src/views/app/LiabilitiesView.tsx
@@ -30,21 +30,27 @@ export default function LiabilitiesView() {
     if(data)
   return (
     <>
-        <div className="flex justify-around">
+        <div className="flex flex-col xl:flex-row justify-around gap-4 mt-5
+            md:gap-10 xl:mt-0">
         <div>
-            <div className="w-[745px] h-[150px] bg-gradient-to-b from-[#ff7b00] to-[#ffb01d] p-5 rounded-2xl space-y-2 flex flex-col justify-center">
-            <h2 className="text-white font-bold text-3xl">Total Liabilities</h2>
-            <p className="text-white font-bold text-3xl">{formatCurrency(total)}</p>
+            <div className="w-full bg-gradient-to-b from-[#ff7b00] to-[#ffb01d] py-3 rounded-2xl space-y-2 flex flex-col justify-center items-center
+                md:p-5 md:h-[150px] lg:items-start">
+            <h2 className="text-white font-bold text-lg
+                md:text-3xl">Total Liabilities</h2>
+            <p className="text-white font-bold text-lg
+                md:text-3xl">{formatCurrency(total)}</p>
             </div>
         </div>
         <div>
-            <div className="w-[745px] h-[150px] bg-gradient-to-b from-[#ff7b00] to-[#ffb01d] p-5 rounded-2xl space-y-2 flex flex-col justify-center">
+            <div className="w-full bg-gradient-to-b from-[#ff7b00] to-[#ffb01d] p-3 rounded-2xl space-y-2 flex flex-col justify-center
+                md:p-5 md:h-[150px]">
                 <CreateLiabilityForm />
             </div>
         </div>
         </div>
 
-        <div className="py-16 grid grid-cols-4 gap-10">
+        <div className="grid grid-cols-2 gap-4 overflow-y-scroll max-h-[380px] mt-10
+            md:max-h-[500px] lg:max-h-[600px] xl:grid-cols-3 2xl:grid-cols-4">
         {data.map( liability => <LiabilityCard key={liability._id} liability={liability}/>)}
         </div>
 
